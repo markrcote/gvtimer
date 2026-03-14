9da2faebaeefd91c811e895e72d835d3ec5d1067
Mark Côté <mark.cote@uwaterloo.ca>
2026-03-14 12:04:36 -0400

    Bump version to 1.2 (versionCode 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.markrcote.gvtimer"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 2
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
