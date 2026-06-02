311e6e6cbb64b16e7b60c6688376d7b168dfcc99
Mark Côté <mark.cote@uwaterloo.ca>
2026-06-01 21:59:39 -0400

    Bump version to 1.4 (versionCode 4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.markrcote.gvtimer"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 4
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
